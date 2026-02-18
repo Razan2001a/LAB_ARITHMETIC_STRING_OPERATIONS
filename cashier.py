76843468db9865d1cc09d43cd4a07b3a901757f4
رزان المطيري <rzanalmtyry@rzans-Laptop.local>
2026-02-18 04:03:09 +0300

    solve python lab

--- cashier.py
+++ cashier.py
@@ -1,1 +1,18 @@
-python3 cashier.py
+price = 3.75
+quantity = 4
+tax_rate = 0.07
+
+subtotal = price * quantity 
+
+tax = subtotal * tax_rate
+
+total = subtotal + tax
+
+print(f"Price  of item: ${price}")
+print(f"Quantity: {quantity}")
+print(f"Tax rate: {tax_rate * 100:.1f}%")
+
+print()
+print(f"Subtotal: ${subtotal:.2f}")
+print(f"Tax: ${tax:.2f}")
+print(f"Total: ${total:.2f}")
---
+++ text_lab.py
@@ -0,0 +1,19 @@
+sentence:str = "I want to learn python programming and become a good developer"
+
+word = "python"
+
+word = "developer"
+
+print(len(sentence))
+
+print(sentence.find(word))
+
+print(sentence.count(word))
+
+print(sentence.upper())
+
+print(sentence.lower())
+
+print(sentence.replace(word, "coding"))
+
+print(sentence[-1])
